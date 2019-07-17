65644a7cb8635c65d9b58df21af3539cdb326319
Hedy Li <50042066+hedyhyry@users.noreply.github.com>
2019-07-17 13:31:00 +0800

    Create factorial-with-func.c

---
+++ Originals/factorial-with-func.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int fac(int n)
+{   int f;  
+    if(n<0)  printf("n<0,data error!");
+    else if(n==0||n==1)  f=1;
+    else f=n*fac(n-1);
+    return f;
+}
+void main()
+{   int n, y;
+    printf("Input a integer number:");
+    scanf("%d",&n);
+    y=fac(n);
+    printf("%d! =%10d\n",n,y);
+}

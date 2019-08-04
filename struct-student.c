3a53ea58b81b8050879531ca4bef67058aa022e6
Hedy Li <50042066+hedyhyry@users.noreply.github.com>
2019-08-04 14:33:58 +0800

    Create struct-student.c

---
+++ struct-student.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+void main()
+{
+	struct student
+	{
+		int num;
+		char name[30];
+		char sex;
+		int age;
+		struct bDate 
+		{
+			int y;
+			int m;
+			int d;
+		}birthday;
+		char addr[100];
+	};
+	//struct student stu1={1,"Hedy Li Haiyue",'f',11,2007,10,4,"B-23-11 Amerin Residence, Selangor, Kuala Lumpur, Malaysia"};
+	struct student stu1;
+	stu1.num=1;
+	strcpy(stu1.name,"Hedy Li Haiyue");
+	stu1.sex='f';
+	stu1.age=11;
+	stu1.birthday.y=2007;
+	stu1.birthday.m=10;
+	stu1.birthday.d=4;
+	strcpy(stu1.addr,"B-23-11 Amerin Residence, Selangor, Kuala Lumpur, Malaysia");
+
+	printf("--Student 1--\nName: %s\nNo.: %d\nSexuality: %c\nAge: %d\nBirthday: %d/%d/%d\nAddress: %s\n\n\n"
+		,stu1.name,stu1.num,stu1.sex,stu1.age,stu1.birthday.d,stu1.birthday.m,stu1.birthday.y,stu1.addr);
+}

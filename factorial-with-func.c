99818c2ecbbddb64f96ff260c5cb283ddd410963
Adithya Sunil Edakkadan <adithyasunil26@gmail.com>
2019-10-06 14:02:12 +0530

    comments added to 10-times-table.c, factorial-with-func.c and is-input-prime.c

--- factorial-with-func.c
+++ factorial-with-func.c
@@ -3,13 +3,13 @@ int fac(int n)
 {   int f;  
     if(n<0)  printf("n<0,data error!");
     else if(n==0||n==1)  f=1;
-    else f=n*fac(n-1);
+    else f=n*fac(n-1);   //recursive function that generates factorial 
     return f;
 }
 void main()
 {   int n, y;
     printf("Input a integer number:");
     scanf("%d",&n);
-    y=fac(n);
+    y=fac(n);   //function call 
     printf("%d! =%10d\n",n,y);
 }

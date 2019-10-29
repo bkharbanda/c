31f500cf5bd9ee2a85b23733c18054f71dc386c2
aryan <bkharbanda@gmail.com>
2019-10-29 19:34:46 +0530

    updated code and added comments

--- descending-digits.c
+++ descending-digits.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 void main()
 {
-    int count=0,a,b,c,d,num;
+    int count=0,a,b,c,d,num;//declaring variables
     printf("Here are the numbers, which units place is bigger than thousands, thousands place is bigger than hundreds and hundreds place is bigger than tens: \n");
     for (num=1000; num<=9999; num++) {
         a=num/1000;
         b=(num-(a*1000))/100;
         c=(num-(b*100)-(a*1000))/10;
         d=num-(c*10)-(b*100)-(a*1000);
-        if (d<=a) {
+        if (d<=a) 
+	{
             continue;
-        }else if (a<=b) {
+        }
+	else if (a<=b) 
+	{
             continue;
-        }else if (b<=c) {
+        }
+	else if (b<=c)
+       	{
             continue;
-        }else {
+        }
+	else
+       	{
             printf("%d\n",num);
             count++;
         }
     }
-    printf("Total: %d\n\n",count);
+    printf("Total such numbers: %d\n",count);
 }
